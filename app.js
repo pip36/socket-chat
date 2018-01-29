a85263f13f28c3a4fc544f30b34034096475f03f
= <=>
2018-01-29 20:16:31 +0000

    Simple styling and user ldeft join messages

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const server = require('http').createServer(app)
 var io = require('socket.io')(server)
 
+app.use("/styles",express.static(__dirname + "/styles"))
+
 const port = process.env.port || 3000
 
 app.get('/', (req, res) => {
@@ -10,11 +12,12 @@ app.get('/', (req, res) => {
 })
 
 io.on('connection', function(socket){
+    io.emit('log message', 'New user has joined!');
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
     })
     socket.on('disconnect', function(){
-
+        io.emit('log message', 'User has left!');
     })
 })
   
